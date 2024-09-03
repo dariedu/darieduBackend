9205867de92cc50ce8e9c82f1b31607d2f809910
Natalia Tkachuk <101522861+Uks130322@users.noreply.github.com>
2024-09-03 20:57:31 +0300

    add: tasks API, swagger for tasks; change: Task model fields (#9)
    Co-authored-by: Alexey <alex.makarov2290@gmail.com>

--- dariedu/task_app/models.py
+++ dariedu/task_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from user_app.models import User
 from address_app.models import RouteSheet, City
+from user_app.models import User
 
 
 class Delivery(models.Model):
@@ -17,23 +17,25 @@ class Delivery(models.Model):
         verbose_name = 'доставка'
         verbose_name_plural = 'доставки'
 
+
 class Task(models.Model):
     category = models.CharField(max_length=255, verbose_name='категория')
     name = models.CharField(max_length=255, verbose_name='название')
     price = models.PositiveIntegerField(verbose_name='часы')
     description = models.TextField(blank=True, null=True, verbose_name='описание')
-    date = models.DateTimeField(verbose_name='дата')
-    duration = models.PositiveIntegerField(verbose_name='длительность')
-    quantity = models.PositiveIntegerField(verbose_name='количество волонтеров')
+    start_date = models.DateTimeField(verbose_name='дата начала')
+    end_date = models.DateTimeField(verbose_name='дата конца')
+    volunteers_needed = models.PositiveIntegerField(verbose_name='требуется волонтеров')
+    volunteers_taken = models.PositiveIntegerField(verbose_name='волонтеров взяли', default=0)
     is_active = models.BooleanField(default=True, verbose_name='активная')
+    is_completed = models.BooleanField(default=False, verbose_name='завершена')
 
     city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True, verbose_name='город')
     curator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='task_curator', verbose_name='куратор')
-    volunteer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='task_volunteer',
-                                  verbose_name='волонтер')
+    volunteers = models.ManyToManyField(User, blank=True, related_name='tasks', verbose_name='волонтеры')
 
     def __str__(self):
-        return f'{self.name}, {self.date}'
+        return f'{self.name}, {self.start_date}'
 
     class Meta:
         verbose_name = 'задание'
