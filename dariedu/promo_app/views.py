fc5d50fa65c8995fa9ef053e5f2b71ce10ab9972
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-09-12 17:33:50 +0300

    Покупка поощрения. Отмена поощрения.

--- dariedu/promo_app/views.py
+++ dariedu/promo_app/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.utils import timezone
-from .models import Promotion
 from .serializers import PromotionSerializer
 from django.db import models
-from .models import Promotion
-from .serializers import PromotionSerializer
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Promotion, User
+from django.core.exceptions import ValidationError
+
 
 
 class PromotionViewSet(viewsets.ModelViewSet):
@@ -15,6 +18,49 @@ class PromotionViewSet(viewsets.ModelViewSet):
     serializer_class = PromotionSerializer
     filterset_fields = ['category', 'city', 'date', 'is_active']
     ordering_fields = ['date', 'price']
+
+    @action(detail=True, methods=['post'], url_path='redeem')
+    def redeem_promotion(self, request, pk):
+        promotion = get_object_or_404(Promotion, pk=pk)
+        user = request.user
+
+        # Проверка достаточности баллов у волонтера
+        if user.point < promotion.price:
+            return Response({'error': 'Недостаточно баллов для приобретения'}, status=400)
+
+        # Уменьшение количества баллов на стоимость поощрения
+        user.point -= promotion.price
+        user.save()
+
+        serializer = PromotionSerializer(instance=promotion, context={'view': self, 'request': request})
+
+        try:
+            # Присвоение поощрения волонтеру
+            return Response({status.HTTP_201_CREATED: serializer.data})
+        except ValidationError as e:
+            return Response({'error': str(e)}, status=400)
+        except Exception as e:
+            return Response({'error': 'Internal Server Error'}, status=500)
+
+    # Функция для отмены поощрения
+    @action(detail=True, methods=['post'], url_path='cancel')
+    def cancel_redeem(self, request, pk):
+        promotion = get_object_or_404(Promotion, pk=pk)
+        user = request.user
+
+        # Возврат баллов волонтеру
+        user.point += promotion.price
+        user.save()
+
+        serializer = PromotionSerializer(instance=promotion, context={'view': self, 'request': request})
+
+        try:
+            # Отмена поощрения в БД
+            return Response({status.HTTP_200_OK: 'Акция успешно отменена, баллы возвращены.'})
+        except ValidationError as e:
+            return Response({'error': str(e)}, status=400)
+        except Exception as e:
+            return Response({'error': 'Internal Server Error'}, status=500)
 
 
 class VolunteerPromotionsView(APIView):
