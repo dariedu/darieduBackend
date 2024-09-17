bb0b5521f015305bd27294be4636430242aa04b5
Natalia Tkachuk <101522861+Uks130322@users.noreply.github.com>
2024-09-18 01:54:06 +0300

    add: curator_of endpoint in tasks_app; change: switched to authentication variant, all fields are readonly in task serializer (#20)
    Co-authored-by: Alexey <alex.makarov2290@gmail.com>

--- dariedu/task_app/permissions.py
+++ dariedu/task_app/permissions.py
@@ -1,10 +1,21 @@
 from rest_framework import permissions
 
+
 # Вернуть по необходимости!
 # Для метода завершения задачи куратором
 class IsAbleCompleteTask(permissions.BasePermission):
     """
     Object-level permission to allow only curator of the task to complete it.
     """
+
     def has_object_permission(self, request, view, obj):
-       return request.user.is_staff and request.user == obj.curator  # TODO swap to comment
+        return request.user.is_staff and request.user == obj.curator
+
+
+class IsCurator(permissions.BasePermission):
+    """
+    Allow access only to curators.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_staff
