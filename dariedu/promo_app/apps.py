29cd10a3845d70b64ac65fb618e0c9ff0017bc44
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-09-19 20:47:26 +0300

    Описание схемы.

--- dariedu/promo_app/apps.py
+++ dariedu/promo_app/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class PromoAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'promo_app'
+
+    def ready(self):
+        import promo_app.schema
