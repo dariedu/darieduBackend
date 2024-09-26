21c52c6c834bb14b8d2f1befe6251e607c82f884
dmitriy <kosheld@inbox.ru>
2024-09-26 20:46:08 +0300

    add Celery, celery-beat, task Notification to the supervisor about a volunteer taking on a task

--- dariedu/dariedu/settings.py
+++ dariedu/dariedu/settings.py
@@ -34,6 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # TODO here is the secret key because JWT can not see it from env. To fix later
 SECRET_KEY = 'django-insecure-i)0i=dqjw4esh4d20@&c(-l(4p9tj)@)08_9vxyc+#%u&33ef$'
 
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -80,6 +82,7 @@ INSTALLED_APPS = [
     'import_export',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -226,3 +229,12 @@ SIMPLE_JWT = {
 }
 
 IMPORT_EXPORT_FORMATS = [XLSX, XLS]
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
+CELERY_BEAT_MAX_INTERVAL = 1
