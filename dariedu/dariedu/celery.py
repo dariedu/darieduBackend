937ecc4c861bbbfcd4586e42a2b436cf615e83e8
Natalia Tkachuk <101522861+Uks130322@users.noreply.github.com>
2024-10-01 17:32:42 +0300

    Task (#27)
    * add Celery, celery-beat, task Notification to the supervisor about a volunteer taking on a task
    
    * add Celery, celery-beat, task Notification to the supervisor about a volunteer taking on a task
    
    * add Celery, celery-beat, task Notification to the supervisor about a volunteer taking on a task
    
    ---------
    
    Co-authored-by: dmitriy <kosheld@inbox.ru>

---
+++ dariedu/dariedu/celery.py
@@ -0,0 +1,15 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dariedu.settings')
+
+app = Celery('dariedu')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.broker_connection_retry_on_startup = True
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
