84b1476b2801c3fe2ba0abb9d9ae2819cf9e5775
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-10-09 14:13:14 +0300

    csrf

--- dariedu/dariedu/settings.py
+++ dariedu/dariedu/settings.py
@@ -124,6 +124,19 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = ["*"]
 CORS_ALLOW_HEADERS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://skillfactory.dariedu.site',
+    'http://skillfactory.dariedu.site',
+    'https://dariedufront.vercel.app',
+    'https://localhost:5173',
+    'http://localhost:5173',
+    'https://localhost:8000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:5173',
+    'https://127.0.0.1:5173',
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
