1a66e24dc1c2653411bebd89910dfb428e060e93
Natalia Tkachuk <101522861+Uks130322@users.noreply.github.com>
2024-10-12 16:37:16 +0300

    Kate (#38)
    * Настройка сигнала при оставлении отзыва.
    
    * Добавлен тип suggestion.
    
    ---------
    
    Co-authored-by: aliensen36 <142628769+aliensen36@users.noreply.github.com>

--- dariedu/feedback_app/signals.py
+++ dariedu/feedback_app/signals.py
@@ -1,9 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
-
 from .models import RequestMessage
 from notifications_app.models import Notification
+from django.conf import settings
+from django.core.mail import send_mail
+from .models import Feedback
 
 
 @receiver(post_save, sender=RequestMessage)
@@ -16,3 +18,24 @@ def create_feedback(sender, instance, created, **kwargs):
             created=timezone.now()
         )
         notification.save()
+
+
+
+@receiver(post_save, sender=Feedback)
+def send_feedback_notification(sender, instance, created, **kwargs):
+    if created:
+        subject = f"Новый отзыв от пользователя {instance.user}"
+        message = (
+            f"Тип отзыва: {instance.get_type_display()}\n"
+            f"Текст: {instance.text}\n"
+            f"Дата создания: {instance.created_at}\n"
+            f"Пользователь: {instance.user.name}"
+        )
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [settings.ADMIN_EMAIL],
+            fail_silently=False,
+        )
