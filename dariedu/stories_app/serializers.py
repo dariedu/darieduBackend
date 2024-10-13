3d067262c4396f4ccdf6656fd6a32fe4df1cf7dc
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-10-14 02:27:03 +0300

    fix stories

--- dariedu/stories_app/serializers.py
+++ dariedu/stories_app/serializers.py
@@ -4,12 +4,10 @@ from .models import Stories
 
 
 class StoriesSerializer(serializers.ModelSerializer):
-    link = serializers.URLField(max_length=500, read_only=True, source='get_link')
 
     class Meta:
         model = Stories
         fields = '__all__'
         extra_kwargs = {
             'id': {'read_only': True},
-            'link': {'read_only': True},
         }
