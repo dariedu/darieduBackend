bea5d1a5e93a5f7d935bd2a7c740d731940711d1
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-10-16 23:36:44 +0300

    Repair connection of celery to redis.

--- dariedu/dariedu/celery.py
+++ dariedu/dariedu/celery.py
@@ -3,6 +3,7 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
+
 logging.basicConfig(level=logging.INFO)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dariedu.settings')
