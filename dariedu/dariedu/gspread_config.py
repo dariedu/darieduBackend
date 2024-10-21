539cec29828e7df7e59c7826e5b4bef2e0678572
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-10-21 21:20:37 +0300

    add icons, correct docker-compose for statics and media, fix search

--- dariedu/dariedu/gspread_config.py
+++ dariedu/dariedu/gspread_config.py
@@ -1,11 +1,14 @@
 import os
 import gspread
+from dotenv import find_dotenv, load_dotenv
 from google.oauth2.service_account import Credentials
 
+load_dotenv(find_dotenv())
 
 SCOPES = os.getenv('SCOPES').split(',')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials.json')
+# SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials.json')
+SERVICE_ACCOUNT_FILE = '/app/credentials.json'  # for docker
 
 credentials = Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE,
