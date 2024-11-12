76dc3793ef2ddd307ff89d57150fedbcf099c992
Andrey <v.andrei.dmitrievich@gmail.com>
2024-11-12 10:02:56 +0300

    beneficiary phone

--- dariedu/address_app/apps.py
+++ dariedu/address_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AddressAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'address_app'
+
+    def ready(self):
+        import address_app.signals
