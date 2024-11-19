ea41323e5445d938fe898b9662655edd8cc23489
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-11-19 15:16:52 +0300

    pictures, urls and volumes

--- dariedu/dariedu/settings.py
+++ dariedu/dariedu/settings.py
@@ -216,11 +216,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = '/usr/src/app/staticfiles'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_ROOT = '/app/media'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/home/app/web/media/'
+MEDIA_DIR = '/usr/src/app/media'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),
--- dariedu/dariedu/urls.py
+++ dariedu/dariedu/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from googleapiclient.channel import notification_from_headers
@@ -60,4 +62,4 @@ urlpatterns = [
     # Optional UI:
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
