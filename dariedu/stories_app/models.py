928a8b523d3c4b9168a1c3119932171bc136c279
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-12-04 15:30:34 +0300

    cover for stories is required

--- dariedu/stories_app/models.py
+++ dariedu/stories_app/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 class Stories(models.Model):
-    cover = models.ImageField(blank=True, null=True, verbose_name='обложка', upload_to='stories_cover',
+    cover = models.ImageField(verbose_name='обложка', upload_to='stories_cover',
             help_text='Обложка для сториса в приложении. Ширина 116, высота 160 пикселей, формат SVG, PNG')
     title = models.CharField(max_length=255, verbose_name='заголовок')
     subtitle = models.CharField(max_length=255, verbose_name='подзаголовок', blank=True, null=True)
