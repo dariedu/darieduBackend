809554cd10ce5e4393c541616f2ead4d9966ae54
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-12-04 23:07:26 +0300

    fix env

--- dariedu/dariedu/settings.py
+++ dariedu/dariedu/settings.py
@@ -43,7 +43,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '0.0.0.0',
     '127.0.0.1',
-] + [os.getenv('ALLOWED_HOSTS')]
+] + os.getenv('ALLOWED_HOSTS').split(',')
 
 CURRENT_HOST = [os.getenv('CURRENT_HOST')]
 
@@ -107,7 +107,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = ["*"]
 CORS_ALLOW_HEADERS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [] + [os.getenv('CSRF_TRUSTED_ORIGINS')]
+CSRF_TRUSTED_ORIGINS = [] + os.getenv('CSRF_TRUSTED_ORIGINS').split()
 
 TEMPLATES = [
     {
