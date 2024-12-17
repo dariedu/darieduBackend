37773aadef4a32e49f2d44112c410030a3c9e144
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-12-17 13:56:48 +0300

    fix admin notifications

--- dariedu/user_app/signals.py
+++ dariedu/user_app/signals.py
@@ -16,14 +16,14 @@ def create_user(sender, instance, created, **kwargs):
         if instance.is_adult:
             notification = Notification.objects.create(
                 title='Новый пользователь',
-                text=f'Зарегистрировался новый пользователь {instance.pk}',
+                text=f'Зарегистрировался новый пользователь {instance}',
                 obj_link=instance.get_absolute_url(),
                 created=timezone.now()
             )
         else:
             notification = Notification.objects.create(
                 title='Несовершеннолетний пользователь',
-                text=f'Зарегистрировался несовершеннолетний пользователь {instance.pk}',
+                text=f'Зарегистрировался несовершеннолетний пользователь {instance}',
                 obj_link=instance.get_absolute_url(),
                 created=timezone.now()
             )
