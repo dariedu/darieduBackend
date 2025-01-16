ee19e6d5f2d4142e7aa4f350b21e055a563e9df6
Andrey <v.andrei.dmitrievich@gmail.com>
2025-01-16 21:15:52 +0300

    fix bug with photo report

--- dariedu/google_drive/google_feedback.py
+++ dariedu/google_drive/google_feedback.py
@@ -1,9 +1,17 @@
+import os
 import datetime
+
+from dotenv import load_dotenv
 
 from google_drive.google_auth import GoogleAuthCustom
 
 
+load_dotenv()
+
+
 class GoogleFeedback(GoogleAuthCustom):
+    FOLDER_ID = os.getenv('FOLDER_ID_PHOTO_REPORT')  # TODO в эту папку будут загружаться фото отчёты
+    EMAIL = os.getenv('GOOGLE_EMAIL')  # TODO необходим для назначения доступа к папке
 
     def feedback_upload(self, file):  # : File
 
@@ -12,14 +20,20 @@ class GoogleFeedback(GoogleAuthCustom):
         today = str(datetime.date.today())
         query = f'title="{today}" and trashed=false'
         folders_list = self.drive.ListFile({'q': query}).GetList()
-        print(folders_list)
         if not folders_list:
             folder_metadata = {
                 'title': today,
-                'mimeType': 'application/vnd.google-apps.folder'
+                'mimeType': 'application/vnd.google-apps.folder',
+                'parents': [{'id': self.FOLDER_ID}]
+            }
+            folder_permission = {
+                'type': 'user',
+                'role': 'writer',
+                'value': self.EMAIL
             }
             folder = self.drive.CreateFile(folder_metadata)
             folder.Upload()
+            folder.InsertPermission(folder_permission)
             folders_list.append(folder)
 
         folder_id = folders_list[0]['id']
