1567b4555bf91887d8e8903fc2a67e1c88779074
dmitriy <kosheld@inbox.ru>
2025-03-01 21:34:32 +0300

    Notifying the manager about the volunteer’s registration for promo. add cancel

--- dariedu/promo_app/signal.py
+++ dariedu/promo_app/signal.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from django.dispatch import receiver
 from django.utils import timezone
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import post_save, post_delete
 from django_celery_beat.models import PeriodicTask, CrontabSchedule
 
 from .models import Promotion, Participation
@@ -35,10 +35,30 @@ def signal_for_promo_users(sender, instance, created, **kwargs):
         name = instance.promotion.name
         notification = Notification.objects.create(
             title='Запись на Поощрение',
-            text=f'Волонтер {user.tg_username} записался на поощрение '
+            text=f'Волонтер {user.tg_username if user.tg_username else user.name} записался на поощрение '
                  f'"{name}"!',
             obj_link=instance.promotion.get_absolute_url(),
         )
         notification.save()
 
-        send_message_to_telegrams.apply_async(args=[instance.promotion.id, user.id], countdown=10)
+        message = f'Волонтер {user.tg_username if user.tg_username else user.name} записался на поощрение "{name}"!'
+
+        send_message_to_telegrams.apply_async(args=[instance.promotion.id, message], countdown=10)
+
+
+@receiver(post_delete, sender=Participation)
+def signal_for_deleted_promo_users(sender, instance, **kwargs):
+    user = instance.user
+    name = instance.promotion.name
+
+    notification = Notification.objects.create(
+        title='Отказ от Поощрения',
+        text=f'Волонтер {user.tg_username if user.tg_username else user.name} отписался от поощрения '
+             f'"{name}"!',
+        obj_link=instance.promotion.get_absolute_url(),
+    )
+    notification.save()
+
+    message = f'Волонтер {user.tg_username if user.tg_username else user.name} отписался от поощрение "{name}"!'
+
+    send_message_to_telegrams.apply_async(args=[instance.promotion.id, message], countdown=10)
